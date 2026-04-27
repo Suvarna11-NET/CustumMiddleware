6f00733e5db071fec98c3732127ed9a09d664ce3
Suvarna <chaudharisuvarna9916@gmail.com>
2026-04-27 22:36:24 +0530

    initial commit

---
+++ CustomMiddleware/Middleware/StudentController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CustomMiddleware.Middleware
+{
+    public class StudentController : Controller
+    {
+        private readonly RequestDelegate _next;
+        public StudentController(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            Console.WriteLine("Before Request");
+
+            await _next(context);
+
+            Console.WriteLine("After Response");
+        }
+
+    }
+}
